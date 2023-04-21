d3c4729739722a5b6e3abf02d1bc2a58bb26bf9f
mojtaba-shahverdi <mojtaba0shahverdi@gmail.com>
2023-04-21 23:15:24 +0430

    withdraw function with modifire

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -12,11 +12,43 @@ contract FundMe {
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     function fund() public payable  {
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value ;
     }  
 
-    // function withdraw(){}
+    function withdraw() public onlyOwner {
+        // without the modifire:
+        // require(msg.sender == owner, "Sender is not Owner");
+
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send Failed");
+
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Sender is not Owner");
+        _;
+    }
 }
