f85c02636c1f243f7e7102071f3a4e56cd1fe187
mojtaba-shahverdi <mojtaba0shahverdi@gmail.com>
2023-04-21 06:12:57 +0430

    AggregatorV3Interface

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,15 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
 
     uint256 public minimumUsd = 50;
 
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
     function fund() public payable  {
-        require(msg.value >= minimumUsd, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
+        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value ;
     }
 
-    
+    function getPrice() public view returns(uint256) {
+        // ABI
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+    }    
+
+    function getConversionRate(uint256 ethAmount) public view  returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }    
 
     // function withdraw(){}
 }
